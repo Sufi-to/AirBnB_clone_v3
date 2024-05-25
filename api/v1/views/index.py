9c0f5fd29925b8fc4f0c86d6cd32a96214501db8
Sufi-to <3thomas225@gmail.com>
2024-05-26 02:56:15 +0400

    push

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Script that contains the routes to the database"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -8,3 +10,14 @@ def status():
     """Returns the status of the api"""
     return jsonify(status='ok')
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """Returns the status of the api"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
