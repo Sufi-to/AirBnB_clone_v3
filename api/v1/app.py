05a9089e6569966691a8dd834b96a8bfa88f41c5
Sufi-to <3thomas225@gmail.com>
2024-05-27 18:08:52 +0400

    error handle

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(os.getenv('HBNB_API_PORT', '5000'))
+port = os.getenv('HBNB_API_PORT', '5000')
 CORS(app, resources={'/*': {'origins': port}})
 
 
@@ -20,12 +20,13 @@ def teardown_db(exception):
     storage.close()
 
 
-# app name
 @app.errorhandler(404)
-def not_found():
+def not_found(error):
     """Handles routes that do not go anywhere"""
-    return jsonify(error="not found"), 404
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
